1f4d6f4c7286094747b37d5f80761a4eeda2876a
bastmaan <bastmaan@live.se>
2025-04-09 07:44:54 +0200

    third commit

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,6 +1,42 @@
 import { Box, Typography, Avatar } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Playlist = ({ spotifyApi, token }) => {
+	const [playlistInfo, setplaylistInfo] = useState();
+	const [songs, setSongs] = useState([]);
+	const { id } = useParams();
+
+	const formatSongs = (items) =>
+		items.map((item, i) => {
+			console.log({ item, i });
+			const {track} = item;
+			track.contextUri = `spotify:playlist:${id}`;
+			track.position = i;
+			return track;
+		});
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const playlistDetails = await spotifyApi.getPlaylist(id);
+				setplaylistInfo({
+					name: playlistDetails.body.name,
+					image: playlistDetails.body.images[0].url
+				});
+				console.log(playlistDetails);
+				const { items } = playlistDetails.body.tracks;
+				// Format Songs
+				const formattedSongs = formatSongs(items);
+				setSongs(formattedSongs);
+			} catch (e) {
+				console.log(e);
+			}
+		};
+
+		getData();
+	}, [id]);
+
 	return (
 		<Box id="Playlist__page" sx={{ backgroundColor: 'background.paper', flex: 1, overflowY: 'auto' }}>
 			<Box
@@ -17,15 +53,15 @@ const Playlist = ({ spotifyApi, token }) => {
 				}}
 			>
 				<Avatar
-					src={null}
+					src={playlistInfo?.image}
 					variant="square"
-					alt={null}
+					alt={playlistInfo?.name}
 					sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235 } }}
 				/>
 				<Box>
 					<Typography sx={{ fontSize: 12, fontWeight: 'bold', color: 'text.primary' }}>Playlist</Typography>
 					<Typography sx={{ fontSize: { xs: 42, md: 72 }, fontWeight: 'bold', color: 'text.primary' }}>
-						Fake Playlistname
+						{playlistInfo?.name}
 					</Typography>
 				</Box>
 			</Box>
